d5bfb91985adc8bd2eb6a625831527469430a06d
Shoaib Sharif <shoaib19j@gmail.com>
2024-09-13 09:41:23 -0500

    fix: esnext

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { Editor } from "./Editor";
+import { sampleJson } from "./sample";
 
 export const Page = () => {
+  const [json] = useState<string>(JSON.stringify(sampleJson));
   return (
     <div className="container mx-auto flex flex-col justify-center h-svh p-7">
-      <Editor />
+      <Editor defaultValue={json} />
     </div>
   );
 };
